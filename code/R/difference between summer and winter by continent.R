dd7f2f9bf1f47f67cd3e0694e5ccaa7ec1d132ac
Alex-work-1 <102028235+Alex-work-1@users.noreply.github.com>
2022-05-25 14:43:01 +0000

    Add files via upload

---
+++ code/R/difference between summer and winter by continent.R
@@ -0,0 +1,62 @@
+install.packages("tidyverse")  
+install.packages("scales")
+install.packages("stringr")
+install.packages("Hmisc") 
+install.packages("forcats")
+install.packages("ggthemes")
+library(data.table)
+library(ggplot2)
+library("tidyverse") 
+library("scales") 
+library("stringr") 
+library("Hmisc") 
+library("forcats") 
+library("ggthemes")
+library(tidyverse)
+library(tidyr)
+colset_4 <-  c("#D35C37", "#BF9A77", "#D6C6B9", "#97B8C2")
+theme_set(theme_bw())
+data_100_years_2 <- data.frame(data_100_years_)
+data_100_years_2[1:16] <- NULL       #####Delete columns and only choosing Summer and Winter runoff means
+view(data_100_years_2)
+colnames(data_100_years_2)           #### Change column names
+names(data_100_years_2)[names(data_100_years_2) == "Summer.mean"] <- "Summer"
+names(data_100_years_2)[names(data_100_years_2) == "Winter.mean"] <- "Winter"
+data_100_years_2
+view(data_100_years_2)
+#Tidy format
+data_summerwintermean2 <- gather(data_100_years_2, Season, Runoff, Summer:Winter, factor_key=TRUE)
+view(data_summerwintermean2)
+#Boxplot of the summer and winter means per station
+ggplot(data_summerwintermean2, aes(x = Season, y = Runoff, fill=Season)) +            
+  geom_boxplot(alpha=0.3) +
+  xlab(label = "Season") +
+  ylab(label = "Runoff (m3/s)") +
+  theme_bw()
+#Not representative because there are a lot of outliers and can't analize the plots 
+#because it is difficult to tell if there are higher runoffs during the winter or summer
+mean(data_100_years_2$Summer)
+mean(data_100_years_2$Winter)
+#Bar plots instead with the mean of summer and winter all over the world
+ggplot(data_summerwintermean2, aes(Season, Runoff)) +           # ggplot2 barplot with mean
+  geom_bar(position = "dodge",
+           stat = "summary",
+           fun = "mean")
+#Differences between runoffs among the continents
+data_100_years_4 <- data.frame(data_100_years_)
+data_100_years_4[1:5] <- NULL 
+data_100_years_4[2:11] <- NULL 
+names(data_100_years_4)[names(data_100_years_4) == "Summer.mean"] <- "Summer"
+names(data_100_years_4)[names(data_100_years_4) == "Winter.mean"] <- "Winter"
+data_summerwintermean4 <- gather(data_100_years_4, Season, Runoff, Summer:Winter, factor_key=TRUE)
+ggplot(data_summerwintermean4, aes(x = Season, y = Runoff, fill=Continent)) +            
+  geom_boxplot(alpha=0.3) +
+  xlab(label = "Season") +
+  ylab(label = "Runoff (m3/s)") +
+  theme_bw() ###Boxplots with the means of summer and winter but not representative because there are too many outliers
+print(data_summerwintermean4)
+print(is.factor(data_summerwintermean4$Continent))
+data_summerwintermean4$Continent <- as.factor(data_summerwintermean4$Continent)
+print(is.factor(data_summerwintermean4$Continent))
+ggplot(data_summerwintermean4, aes(fill=Continent, y=Runoff, x=Season)) + 
+  geom_bar(position="dodge", stat="identity", fun = "mean")   ### Barplots of the different runoff in the continents
